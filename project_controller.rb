818d865056c187048075064e7d2ffb05ea3b962a
shingara <cyril.mougel@gmail.com>
2008-04-14 23:19:05 +0200

    Ajout de la presentation des tests et exemples de view

---
+++ project_controller.rb
@@ -0,0 +1,13 @@
+class ProjectController < ApplicationController
+
+  # http://localhost:3000/projects
+  def index
+    @projects = Project.find :all
+  end
+
+  # http://localhost:3000/projects/1
+  def show
+    @project = Project.find params[:id]
+  end
+
+end
