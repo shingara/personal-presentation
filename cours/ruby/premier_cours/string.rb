1ad026abb706dacfc67ddf1422d021dc0fe48639
Cyril Mougel <cyril.mougel@gmail.com>
2010-01-09 13:45:05 +0100

    add string in cours

---
+++ cours/ruby/premier_cours/string.rb
@@ -0,0 +1,5 @@
+puts "Hello World"  # Hello World
+puts 'Hello World'  # Hello World
+puts 'I like' + ' Ruby' # I like Ruby
+puts 'It\'s my Ruby'   # It's my ruby
+puts 'Hello' * 3  # HelloHelloHello
