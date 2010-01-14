2bfc878e09636fbb4506744fe2d601bb4804737e
Cyril Mougel <cyril.mougel@gmail.com>
2010-01-14 12:19:00 +0100

    add variable part

--- cours/ruby/premier_cours/variable.rb
+++ cours/ruby/premier_cours/variable.rb
@@ -22,21 +22,3 @@ end
 
 Hello.bar('bar')
 puts Hello.baz # baz
-
-class Hello
-
-  def bar(foo)
-    @foo = foo
-  end
-
-  def baz
-    @foo
-  end
-end
-
-
-
-hello = Hello.new
-hello.bar('baz')
-puts hello.baz # baz
-
