4da48ecd245b86265a0937ed1abc3e17d726ad96
Cyril Mougel <cyril.mougel@gmail.com>
2010-02-18 17:17:45 +0100

    add information about ensure

--- cours/ruby/exercices/car.rb
+++ cours/ruby/exercices/car.rb
@@ -1,65 +1,100 @@
+module Driver
+
+  def has_driver?
+    @has_driver
+  end
+
+  def add_driver
+    @has_driver = true
+  end
+
+end
+
+class Fixnum
+  def cars
+    acc = []
+    self.times{|v|
+      acc << Car.new
+    }
+    acc
+  end
+end
+
+class Hash
+  def generate_parkings
+    parks = []
+    self.each { |k,v|
+      parks << Parking.new(v,k)
+    }
+    parks
+  end
+end
+
+class Parking < Array
+  attr_reader :name
+  attr_reader :max_size
+  def initialize(max_cars, name='')
+    @max_size = max_cars
+    @name = name
+  end
+
+  def cars
+    self
+  end
+
+  def <<(car)
+    if size < @max_size &&
+      !include?(car)
+      super car
+    end
+  end
+
+  def out(car)
+    delete(car)
+  end
+
+end
 
 class Car
 
-  attr_reader :nb_stop
-  attr_reader :first_passenger, :second_passenger
+  attr_reader :nb_stop, :marque
 
-  def initialize(nb_running=0)
-    @nb_stop = nb_running
-    self.class.add_car
+  include Driver
+
+  def initialize(nb=0)
+    @nb_stop = nb
+    @@nb_car ||= 0
+    @@nb_car += 1
+    @marque = self.class.to_s
   end
 
   def running?
-    @run == true
+    @running
   end
 
   def run
-    @run = true
-    if block_given?
-      yield self
-      @run = false
-    end
+    @running = true
   end
 
   def stop
-    @nb_stop += 1 if running?
-    @run = false
+    @running = false
+    @nb_stop += 1
   end
 
+  def self.nb_car
+    @@nb_car
+  end
+
+  attr_reader :first_passenger
+  attr_reader :second_passenger
   def passenger(first, second=nil)
-    @first_passenger = first
-    @second_passenger = second
+   @first_passenger = first
+   @second_passenger = second
   end
 
-  class << self
-    def nb_car
-      @@nb_car
-    end
-
-    def add_car
-      @@nb_car ||= 0
-      @@nb_car += 1
-    end
-  end
-
-end
-
-module Driver
-  def add_driver
-    @driver = true
-  end
-
-  def has_driver?
-    @driver
-  end
 end
 
 class Ford < Car
-  include Driver
-  def marque
-    'Ford'
-  end
-
   def run
     super
     'boom'
@@ -67,50 +102,5 @@ class Ford < Car
 end
 
 class Renault < Car
-  include Driver
-  def marque
-    'Renault'
-  end
-end
-
-class Parking < Array
-
-  attr_accessor :cars
-  attr_reader :max_size, :name
-
-  def initialize(nb_max_car, name='park_1')
-    @max_size = nb_max_car
-    @cars = []
-    @name = name
-  end
-
-  def <<(car)
-    @cars << car if @max_size > @cars.size && !@cars.include?(car)
-  end
-
-  def out(car)
-    @cars.delete_if{|c| c == car}
-  end
-
-end
-
-class Integer
-  def cars
-    cars = []
-    self.times do |c|
-      cars << Car.new
-    end
-    cars
-  end
-end
-
-class Hash
-  def generate_parkings
-    parks = []
-    self.each do |k,v|
-      parks << Parking.new(v, k)
-    end
-    parks
-  end
 end
 
--- cours/ruby/sample/yaml.rb
+++ cours/ruby/sample/yaml.rb
@@ -1,8 +1,16 @@
 require 'yaml'
 
-foo = ['bar', 'baz', {:hello => 'foo'}]
+class Car
+  def initialize(name)
+    @name = name
+  end
+  attr_reader :name
+end
+
+foo = Car.new('hello')
 File.open('yaml.yaml', 'w+') do |f|
   YAML.dump(foo, f)
 end
 ok = YAML.load_file('yaml.yaml')
 p ok
+p ok.name
